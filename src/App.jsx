619601545689fcd8e237c19a1d81e1715338be84
Shevraar <58468371+GabrieleZito@users.noreply.github.com>
2025-12-04 11:09:13 +0100

    refactor: clean up App component and update Vite config for path aliasing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import tmdbAPI from "./api/movieDB";
 
 function App() {
-    useEffect(() => {
-        const fetchAPI = async () => {
-            const data = await tmdbAPI.getTVSeriesgenres();
-            console.log(data);
-        };
-        fetchAPI();
-    }, []);
-    return <></>;
+
+    return (
+        <>
+        </>
+    );
 }
 
 export default App;
