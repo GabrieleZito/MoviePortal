f14925f5b669fb29380b280ae6a82fdf723a7ca2
Shevraar <58468371+GabrieleZito@users.noreply.github.com>
2025-12-04 14:45:40 +0100

    feat: implement navigation and page components for Film, TV, Favorites, Account, and Contacts

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,69 +1,66 @@
 import { useState } from "react";
 import "./NavBar.css";
 
-export default function NavBar() {
-const [showGenres, setShowGenres] = useState(false);
-const [darkMode, setDarkMode] = useState(false);
+export default function NavBar({ changePage }) {
+    const [showGenres, setShowGenres] = useState(false);
+    const [darkMode, setDarkMode] = useState(false);
 
-const genres = [
-"Crime", "Horror", "Fantascienza", "Comici",
-"Animazione", "Drammatici", "Fantasy",
-"Azione", "Thriller", "Sport", "Anime"
-];
+    const genres = [
+        "Crime",
+        "Horror",
+        "Fantascienza",
+        "Comici",
+        "Animazione",
+        "Drammatici",
+        "Fantasy",
+        "Azione",
+        "Thriller",
+        "Sport",
+        "Anime",
+    ];
 
-return (
-<nav className={`navbar ${darkMode ? "dark" : ""}`}>
-{/* LOGO */}
-<div className="navbar-left">
-<div className="logo">ZAP Channel</div>
+    return (
+        <nav className={`navbar ${darkMode ? "dark" : ""}`}>
+            {/* LOGO */}
+            <div className="navbar-left">
+                <div className="logo">ZAP Channel</div>
 
-{/* SEARCH */}
-<div className="search-box">
-<input type="text" placeholder="cosa vuoi cercare..." />
-<button>Cerca</button>
-</div>
-</div>
+                {/* SEARCH */}
+                <div className="search-box">
+                    <input type="text" placeholder="cosa vuoi cercare..." />
+                    <button>Cerca</button>
+                </div>
+            </div>
 
-{/* LINKS */}
-<ul className="navbar-links">
-<li>Home</li>
-<li>Film</li>
-<li>Serie TV</li>
-<li>Preferiti</li>
-<li>Contatti</li>
-<li>Account</li>
-<li>Esci</li>
-</ul>
+            {/* LINKS */}
+            <ul className="navbar-links">
+                <li onClick={() => changePage(1)}>Film</li>
+                <li onClick={() => changePage(2)}>Serie TV</li>
+                <li onClick={() => changePage(3)}>Preferiti</li>
+                <li onClick={() => changePage(4)}>Contatti</li>
+                <li onClick={() => changePage(5)}>Account</li>
+                <li>Esci</li>
+            </ul>
 
-{/* GENERI + DARK MODE */}
-<div className="navbar-right">
+            {/* GENERI + DARK MODE */}
+            <div className="navbar-right">
+                <div className="genres" onMouseEnter={() => setShowGenres(true)} onMouseLeave={() => setShowGenres(false)}>
+                    Generi ▾
+                    {showGenres && (
+                        <div className="genres-menu">
+                            {genres.map((g) => (
+                                <div key={g}>{g}</div>
+                            ))}
+                        </div>
+                    )}
+                </div>
 
-<div
-className="genres"
-onMouseEnter={() => setShowGenres(true)}
-onMouseLeave={() => setShowGenres(false)}
->
-Generi ▾
-
-{showGenres && (
-<div className="genres-menu">
-{genres.map((g) => (
-<div key={g}>{g}</div>
-))}
-</div>
-)}
-</div>
-
-{/* SWITCH */}
-<label className="switch">
-<input
-type="checkbox"
-onChange={() => setDarkMode(!darkMode)}
-/>
-<span className="slider"></span>
-</label>
-
-</div>
-</nav>
-);
+                {/* SWITCH */}
+                <label className="switch">
+                    <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
+                    <span className="slider"></span>
+                </label>
+            </div>
+        </nav>
+    );
 }
---
+++ src/Components/NavBarTV/NavBarTV.jsx
@@ -0,0 +1,3 @@
+export default function NavBarTV() {
+    return <div>NavBarTV</div>;
+}
