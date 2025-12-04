77c7eb50371b4b879814be6632bf9bf8f6d0214c
Shevraar <58468371+GabrieleZito@users.noreply.github.com>
2025-12-04 18:05:38 +0100

    roba

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
-import NavBar from "./Components/NavBar";
+import NavBar from "./Components/NavBar.jsx";
 //import FilmPage from "./components/FilmPage/FilmPage";
-import TVPage from "./components/TVPage/TVPage";
-import FavoritePage from "./components/FavoritePage/FavoritePage";
+import TVPage from "./components/TVPage/TVPage.jsx";
+import FavoritePage from "./components/FavoritePage/FavoritePage.jsx";
 import { useLocalStorage } from "./hooks/useLocalStorage";
-import AccountPage from "./components/AccountPage/AccountPage";
-import ContactsPage from "./components/ContactsPage/ContactsPage";
-import Footer from "./components/Footer/Footer";
+import AccountPage from "./components/AccountPage/AccountPage.jsx";
+import ContactsPage from "./components/ContactsPage/ContactsPage.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
     const [page, setPage] = useState(1);
-    const [favorites, setFavorites, addFavorite, removeFavorite] = useLocalStorage("favorites", []);
+    //const [favorites, setFavorites, addFavorite, removeFavorite] = useLocalStorage("favorites", []);
     let renderPage = <div> BOH</div>;
 
     if (page == 1) {
@@ -18,7 +18,7 @@ function App() {
     } else if (page == 2) {
         renderPage = <TVPage />;
     } else if (page == 3) {
-        renderPage = <FavoritePage favorites={favorites} />;
+        renderPage = <FavoritePage />;
     } else if (page == 4) {
         renderPage = <ContactsPage />;
     } else if (page == 5) {
