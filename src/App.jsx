64ac30ac6ed475bce66f04ca94bbc07dc3c3fe05
AndreaPetri11 <andreapetri11@gmail.com>
2025-12-05 10:49:00 +0100

    trying to fix import navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 //import { useLocalStorage } from "./hooks/useLocalStorage";
-import Navbar from "./components/Navbar";
+import Navbar from "./Components/Navbar.jsx";
 import FilmPage from "./components/FilmPage/FilmPage.jsx";
 import TVPage from "./components/TVPage/TVPage.jsx";
 import FavoritePage from "./components/FavoritePage/FavoritePage.jsx";
